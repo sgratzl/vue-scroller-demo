a16eba8a9853b3dabb906041d7b88f110c5ce2d5
Samuel Gratzl <sam@sgratzl.com>
2019-09-25 09:14:06 -0400

    more options

--- src/components/VirtualTable.vue
+++ src/components/VirtualTable.vue
@@ -2,6 +2,7 @@
   <table>
     <thead>
       <tr>
+        <th></th>
         <th v-for="c in colNames" :key="c">
           {{c}}
         </th>
@@ -9,9 +10,10 @@
     </thead>
     <tbody>
       <tr v-for="(row, i) in cells" :key="i">
-        <th v-for="(v, j) in row" :key="j">
+        <th>{{rowNames[i]}}</th>
+        <td v-for="(v, j) in row" :key="j">
           {{v}}
-        </th>
+        </td>
       </tr>
     </tbody>
   </table>
@@ -27,26 +29,69 @@ export default {
     rows: {
       type: Number,
       required: true
+    },
+    missingPercentage: {
+      type: Number,
+      default: 0.1
+    },
+    catColumnPercentage: {
+      type: Number,
+      default: 0.1
     }
   },
 
   computed: {
     colNames () {
-      return Array.from({ length: this.cols }).map((_, i) => i.toString())
+      return Array.from({ length: this.cols }).map((_, i) => this.toBase26(i))
     },
     rowNames () {
-      return Array.from({ length: this.rows }).map((_, i) => i.toString())
+      return Array.from({ length: this.rows }).map((_, i) => (i + 1).toString())
     },
     cells () {
       const cells = []
+      const isCategory = this.colNames.map(() => Math.random() < this.catColumnPercentage)
       for (let i = 0; i < this.rows; ++i) {
         const row = []
         for (let j = 0; j < this.cols; ++j) {
-          row.push(Math.random())
+          if (row === 0) {
+            row.push(this.generateLabel(j))
+          } else if (isCategory[j]) {
+            row.push(this.generateCategory())
+          } else {
+            row.push(this.generateValue())
+          }
         }
         cells.push(row)
       }
       return cells
+    }
+  },
+
+  methods: {
+    toBase26 (v) {
+      let r = []
+      let vi = v
+      const encode = v => String.fromCharCode('A'.charCodeAt(0) + v)
+      while (vi >= 26) {
+        r.unshift(encode(Math.floor(vi / 26)))
+        vi = vi % 26
+      }
+      r.unshift(encode(vi))
+      return r.join('')
+    },
+    generateValue () {
+      const isMissing = Math.random() < this.missingPercentage
+      if (isMissing) {
+        return null
+      }
+      return Math.random().toFixed(3)
+    },
+    generateLabel (i) {
+      return `Col${i}`
+    },
+    generateCategory () {
+      const cats = ['Cat1', 'Cat2', 'Cat3']
+      return cats[Math.min(Math.floor(Math.random() * cats.length), cats.length - 1)]
     }
   }
 }
